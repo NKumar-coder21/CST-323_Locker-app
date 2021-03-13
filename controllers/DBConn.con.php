6255c2d49112003bb2dc94b6fdf9846467d11ebe
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2021-03-13 15:27:40 -0600

    DBConn for Azure

--- controllers/DBConn.con.php
+++ controllers/DBConn.con.php
@@ -1,19 +1,15 @@
 <?php
 //  <!--Created by Nathaniel Kumar @ GCU 2021 -->
 //connect to the database
-$servername = "localhost";
-$DBuser = "root";
-$DBpass = "root";
-$DBname = "DBLock";
-$port = "8889";
-$DBsock = "/Applications/MAMP/tmp/mysql/mysql.sock";
+$servername = "127.0.0.1";
+$DBuser = "azure";
+$DBpass = "6#vWHD_$";
+$DBname = "localdb";
 $success = mysqli_connect(
     $servername,
     $DBuser,
     $DBpass,
-    $DBname,
-    $port,
-    $DBsock
+    $DBname
 );
 if (!$success) {
     die("Connection to DB failed: " . mysqli_connect_error());
