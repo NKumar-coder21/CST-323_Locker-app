0bf9c18600a8d657e4eb0c0bcacd3f7c708eead9
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2021-03-13 15:33:21 -0600

    Update DBConn.con.php

--- controllers/DBConn.con.php
+++ controllers/DBConn.con.php
@@ -5,11 +5,13 @@ $servername = "127.0.0.1";
 $DBuser = "azure";
 $DBpass = "6#vWHD_$";
 $DBname = "localdb";
+$DBport = "52744";
 $success = mysqli_connect(
     $servername,
     $DBuser,
     $DBpass,
-    $DBname
+    $DBname,
+    $DBport
 );
 if (!$success) {
     die("Connection to DB failed: " . mysqli_connect_error());
